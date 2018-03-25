60a278d67c0dce1e026a2090160f2b742bcca0e9
Vikram Venkatarmanan <vikram.venkataramanan@mail.utoronto.ca>
2018-03-25 01:25:55 -0400

    Added Generated Tests!

--- Tests.hs
+++ Tests.hs
@@ -1,14 +1,22 @@
-import           Test.QuickCheck (quickCheck)
+import           Control.Monad   (liftM, liftM2)
+import           Data.Set        as Set
+import           Test.QuickCheck
 import           TypeChecker
-import           Data.Set                       as Set
 
 -- Expected error messages. DON'T CHANGE THESE!
+errorIfBranches :: String
 errorIfBranches = "Type error: the two branches of an `if` must have the same type."
+errorIfCondition :: String
 errorIfCondition = "Type error: the condition of an `if` must be boolean."
+errorCallNotAFunction :: String
 errorCallNotAFunction = "Type error: first expression in a function call must be a function."
+errorCallWrongArgNumber :: String
 errorCallWrongArgNumber = "Type error: function called with the wrong number of arguments."
+errorCallWrongArgType :: String
 errorCallWrongArgType = "Type error: function called with an argument of the wrong type."
+errorUnboundIdentifier :: String
 errorUnboundIdentifier = "Error: unbound identifier."
+errorTypeUnification :: String
 errorTypeUnification = "Type error: inconsistent set of type constraints generated during type inference."
 
 -- | Sample tests for `If`.
@@ -75,44 +83,97 @@ test_DefineTwo =
 - t1 and t2 are Bool_
 - t1 and t2 are Function types
 -}
+
 test_UnifyT1TypeVar =
-  (unify (TypeVar "a") Int_) == Just (Set.fromList [(TypeVar "a", Int_)])
+  unify (TypeVar "a") Int_ == Just (Set.fromList [(TypeVar "a", Int_)])
 test_UnifyT2TypeVar =
-  (unify Bool_ (TypeVar "b")) == Just (Set.fromList [(Bool_, TypeVar "b")])
+  unify Bool_ (TypeVar "b") == Just (Set.fromList [(Bool_, TypeVar "b")])
 test_UnifyPrimitivesInt =
-  (unify Int_ Int_) == Just Set.empty
+  unify Int_ Int_ == Just Set.empty
 test_UnifyPrimitivesBool =
-  (unify Bool_ Bool_) == Just Set.empty
+  unify Bool_ Bool_ == Just Set.empty
 test_UnifyFunctionCanUnifySimple =
-  (unify (Function [(TypeVar "a")] Int_) (Function [Int_] Int_)) ==
+  unify (Function [TypeVar "a"] Int_) (Function [Int_] Int_) ==
     Just (Set.fromList [(TypeVar "a", Int_)])
 test_UnifyFunctionCanUnifyLonger =
-  (unify
-    (Function [(TypeVar "a"), (TypeVar "b")] Int_)
-    (Function [Int_, Int_] Int_)) ==
+  unify
+    (Function [TypeVar "a", TypeVar "b"] Int_)
+    (Function [Int_, Int_] Int_) ==
     Just (Set.fromList [(TypeVar "a", Int_), (TypeVar "b", Int_)])
+
 test_UnifyFunctionCanUnifyEvenLonger =
-  (unify
-    (Function [(TypeVar "a"), (TypeVar "b"), (TypeVar "c")] (TypeVar "r"))
-    (Function [Int_, Int_, Bool_] Bool_)) ==
+  unify
+    (Function [TypeVar "a", TypeVar "b", TypeVar "c"] (TypeVar "r"))
+    (Function [Int_, Int_, Bool_] Bool_) ==
     Just (Set.fromList [(TypeVar "r", Bool_), (TypeVar "a", Int_), (TypeVar "b", Int_), (TypeVar "c", Bool_)])
+
+
+{----------------------------------------------------------------------------------------
+Some more test cases!
+----------------------------------------------------------------------------------------}
+
+--- Let quickcheck generate the text cases
+instance Arbitrary Type where
+    arbitrary =
+        oneof [
+            return Int_
+            , return Bool_
+            , fmap TypeVar arbitrary
+            , liftM2 Function (return []) arbitrary
+            , liftM2 Function (resize 3 (sized (\x -> listOf arbitrary))) arbitrary
+        ]
+
+test_UnifyTypeVar :: String -> Type -> Bool
+test_UnifyTypeVar t s =
+    unify (TypeVar t) s == Just (Set.fromList [(TypeVar t, s)])
+    &&
+    unify s (TypeVar t) == Just (Set.fromList [(s, TypeVar t)])
+
+
+-- Tried to generate another test case for unify,
+-- but seemed like would basically rewrite code here.
+
+-- test_UnifyFunction :: [String] -> String  -> [Type] -> Type -> Property
+-- test_UnifyFunction argsNames retName argsVals retVal =
+--     let
+--         typeVarArgs = fmap TypeVar argsNames
+--         typeVarRet  = TypeVar retName
+--         pairs = zip typeVarArgs argsVals
+--         in (length typeVarArgs == length argsVals && length argsVals  > 2) ==>
+--             unify (Function typeVarArgs typeVarRet) (Function argsVals retVal)
+--             == Just (Set.fromList pairs)
+
+-----------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
+    putStrLn "\n=== typeCheck Tests ==="
     quickCheck test_IfCorrect
     quickCheck test_IfBadCondition
     quickCheck test_IfBadBranches
     quickCheck test_IfSubExprError
+
     quickCheck test_CallCorrect
     quickCheck test_CallNotAFunction
     quickCheck test_CallWrongArgNumber
     quickCheck test_CallWrongArgType
+
     quickCheck test_DefineOne
     quickCheck test_DefineTwo
+
+
+    putStrLn "\n=== unify Tests ==="
     quickCheck test_UnifyT1TypeVar
     quickCheck test_UnifyT2TypeVar
+    quickCheck test_UnifyTypeVar
+
     quickCheck test_UnifyPrimitivesInt
     quickCheck test_UnifyPrimitivesBool
+
     quickCheck test_UnifyFunctionCanUnifySimple
     quickCheck test_UnifyFunctionCanUnifyLonger
     quickCheck test_UnifyFunctionCanUnifyEvenLonger
+
+    putStrLn "\n"
+
+-----------------------------------------------------------------------------------------
